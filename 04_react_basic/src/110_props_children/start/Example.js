2cd56db15a60cf8729eb07ee21fe7b9903a7f5a5
murakami <murakami@kokochie.jp>
2023-10-31 23:54:37 +0900

    props

--- 04_react_basic/src/110_props_children/start/Example.js
+++ 04_react_basic/src/110_props_children/start/Example.js
@@ -2,14 +2,28 @@ import Profile from "./components/Profile";
 import Container from "./components/Container";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
+  { name: "Takashi", age: 19, country: "Japan", color: "" },
   { name: "Jane", age: 28, country: "UK", color: "blue" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container title="Childrenとは？" >
+        <Profile {...profile[0]} />
+        <Profile {...profile[1]} />
+      </Container>
+      <Container title="Childrenとは？"  
+        children={
+          [
+            <Profile key={profile[0].name} {...profile[0]} />,
+            <Profile key={profile[1].name} {...profile[1]} />
+          ]
+      }
+      first={
+        <Profile key={profile[0].name} {...profile[0]} />
+      }
+      />
     </div>
   );
 };

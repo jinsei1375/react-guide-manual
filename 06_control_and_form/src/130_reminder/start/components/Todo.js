42b5ac4239d9b95566fc21a0883f7fa3ae734686
murakami <murakami@kokochie.jp>
2023-11-05 20:33:43 +0900

    todoアプリwip

---
+++ 06_control_and_form/src/130_reminder/start/components/Todo.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Form from "./Form";
+import List from "./List";
+
+const Todo = () => {
+  const todosList = [
+    {
+      id: 1,
+      content: "店予約する",
+    },
+    {
+      id: 2,
+      content: "卵買う",
+    },
+    {
+      id: 3,
+      content: "郵便出す",
+    },
+  ];
+  // return (
+  //   todosList.map(todoList => {
+  //     return <li key={todoList.id}><button onClick={complete}>完了</button>{todoList.content}</li>
+  //   })
+  // );
+
+  const [todos, setTodos] = useState(todosList);
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodos(newTodos);
+  }
+  return (
+    <>
+      <List todos={todos} deleteTodo={deleteTodo} />
+      <Form />
+    </>
+  )
+};
+
+export default Todo;

78d6e224a709422cb61f1a74eb25bbdc2d478bde
murakami <murakami@kokochie.jp>
2023-11-08 23:43:51 +0900

    usededuce

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
@@ -1,7 +1,39 @@
+import { useReducer, useState } from "react";
 const Example = () => {
+  const [state, setState] = useState(0);
   
+  const [rstate, dispatch] = useReducer((prev, {type, step}) => {
+    switch(type) {
+      case '+':
+        return prev + step;
+      case '-':
+        return prev - step;
+      default:
+        throw new Error('エラー');
+    }
+    // if(action === '+') {
+    //   return ++prev;
+    // } else if (action === '-') {
+    //   return --prev;
+    // }
+  }, 0);
+
+  const countUp = () => {
+    return setState(prev => ++prev);
+  }
+  const rcountUp = () => {
+    dispatch({ type: "+", step: 2 });
+  }
+  const rcountDown = () => {
+    dispatch({ type: "-", step: 3 });
+  }
   return (
     <>
+      <h3>{ state }</h3>
+      <button onClick={countUp}>+</button>
+      <h3>{ rstate }</h3>
+      <button onClick={rcountUp}>+</button>
+      <button onClick={rcountDown}>-</button>
     </>
   );
 };

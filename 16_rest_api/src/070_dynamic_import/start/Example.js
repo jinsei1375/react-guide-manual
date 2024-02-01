c2241db6acc4b58ac87177efe117180ef4cc5a3d
murakami <murakami@kokochie.jp>
2024-02-01 23:01:03 +0900

    ダイナミックインポート

--- 16_rest_api/src/070_dynamic_import/start/Example.js
+++ 16_rest_api/src/070_dynamic_import/start/Example.js
@@ -1,5 +1,16 @@
+import { add } from "./add";
+
 const Example = () => {
-  
+  import("./add").then(module => {
+    console.log(module);
+  })
+
+  const dynamicImport = async () => {
+    const module = await import("./add");
+    console.log(module);
+  }
+  dynamicImport();
+  // console.log(add(1,2));
 };
 
 export default Example;

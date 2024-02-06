f3c3b963f33fad207d2aff1cefce1e6953ca0466
murakami <murakami@kokochie.jp>
2024-02-06 23:24:18 +0900

    next.js router

--- 17_nextjs_p1/start/src/pages/index.js
+++ 17_nextjs_p1/start/src/pages/index.js
@@ -1,9 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Home</h1>
-  )
+    <>
+      <h1>Home</h1>
+      <Link href={{ pathname: "/07_router", query: { key: "value" } }}>
+        <a>/07_router</a>
+      </Link>
+      <Link href="/07_router/hello/setting">
+      <a>/07_router</a>
+      </Link>
+    </>
+  );
 }

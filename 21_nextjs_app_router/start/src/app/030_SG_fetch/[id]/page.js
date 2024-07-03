2faa1c1ab1096aba2a370002746aac83a569ce18
Shinsei <murakami.shinsei.zn@gmail.com>
2024-07-03 23:01:26 +0900

    メタデータ設定

--- 21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
+++ 21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
@@ -1,6 +1,19 @@
 import { ENDPOINT } from '@/constants';
 import Article from '../../../components/article';
 
+export async function generateMetadata({ params }) {
+  const article = await fetch(`${ENDPOINT}/${params.id}`).then((res) => res.json());
+  return {
+    title: article.title,
+    description: article.text,
+  };
+}
+
+// export const metadata = {
+//   title: 'test',
+//   description: 'test',
+// };
+
 export async function generateStaticParams() {
   const data = await fetch(ENDPOINT).then((res) => res.json());
   console.log(data);
